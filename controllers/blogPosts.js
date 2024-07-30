67193a13c8e726da735d688fd460ab8975edf121
Ryan Sherman <rsherman5@elon.edu>
2024-07-30 19:15:36 -0400

    sync auth template and finish create functionality

--- controllers/blogPosts.js
+++ controllers/blogPosts.js
@@ -3,6 +3,28 @@ const verifyToken = require('../middleware/verify-token.js');
 const BlogPost = require('../models/blogPost.js');
 const router = express.Router();
 
-router.post('/', verifyToken, (req, res) => {
-    //code here
+
+//Public Routes (for users and guests)
+
+router.get('/', (req, res) => {
+
+});   
+
+
+
+
+// Protected Routes (only for users)
+
+router.use(verifyToken);
+
+router.post('/', async (req, res) => {
+    try {
+        req.body.author = req.user._id;
+        const blogPost = await BlogPost.create(req.body);
+        blogPost._doc.author = req.user;
+        res.status(201).json(blogPost);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
 });
